8e587a12dc65e42eda05a10e3313743ebe97b826
faasaal <kpfasal455@gmail.com>
2025-09-03 12:25:31 +0530

    add

--- lib/screens/uuid.dart
+++ lib/screens/uuid.dart
@@ -2,4 +2,4 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:uuid/uuid.dart';
 
 final id = const Uuid().v4();
-final ref = FirebaseStorage.instance.ref().child('pets').child('$id.jpg');
+final ref = FirebaseStorage.instance.ref().child('pets').child('$id.jpg');;--------------
